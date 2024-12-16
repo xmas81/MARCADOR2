b865211d4e9854f4f8c3c8e00846c3725bd2166d
xisco <xmas@iesdamiahuguet.net>
2024-12-16 12:14:08 +0100

    Primeres passes

--- funcions.js
+++ funcions.js
@@ -11,3 +11,11 @@ function marcador(equip, punts) {
     }
     document.getElementById('historic').innerHTML += "<p>" + puntsA + "-" + puntsB + "</p>";
 }
+
+function inici() {
+    puntsA = 0;
+    puntsB = 0;
+    document.getElementById('historic').innerHTML = "<h2>Historial de la Partida</h2>";
+    document.getElementById('points-a').innerHTML = 0;
+    document.getElementById('points-b').innerHTML = 0;
+}
